194339745d7579f3253185684282dbf9f93ed567
Gaku Gaku <myrgksshuri@gmail.com>
2022-02-27 22:17:46 +0900

    G26 PINに通電するとビープ音がなるコード

--- bilibili_emb/src/main.cpp
+++ bilibili_emb/src/main.cpp
@@ -1,9 +1,23 @@
 #include <M5StickCPlus.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int PIN = 26;
+
+void setup()
+{
+  M5.begin();
+  pinMode(PIN, INPUT_PULLUP);
+  M5.Beep.setBeep(400, 100); // ビープ音設定(初期値)
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  M5.update();
+
+  bool isPressed = !digitalRead(PIN);
+  if (isPressed)
+  {
+    M5.Beep.beep();
+  }
+
+  delay(2);
 }
