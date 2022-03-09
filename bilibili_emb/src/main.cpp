bd5be7ab655115cadca5e3e2d935f147d2aa8685
Gaku Gaku <myrgksshuri@gmail.com>
2022-03-10 00:26:54 +0900

    棒接触時に音を鳴らす

--- bilibili_emb/src/main.cpp
+++ bilibili_emb/src/main.cpp
@@ -1,23 +1,39 @@
 #include <M5StickCPlus.h>
 
-const int PIN = 26;
+const uint32_t PIN = 26;
+const uint32_t MARGIN_TIME = 1000; // 接触時の無敵時間 [ms]
+
+Button stickBtn = Button(PIN, false, 5); // イライラ棒の接触判定ボタン
+uint32_t pressCount;                     // 接触回数
+uint32_t pressTime;                      // 接触からの無敵時間用一時変数 [ms]
 
 void setup()
 {
   M5.begin();
   pinMode(PIN, INPUT_PULLUP);
-  M5.Beep.setBeep(400, 100); // ビープ音設定(初期値)
+
+  pressCount = 0;
+  pressTime = 0;
+
+  // ビープ音設定(初期値)
+  M5.Beep.setBeep(300, 1000);
+  M5.Beep.setVolume(11);
 }
 
 void loop()
 {
   M5.update();
+  stickBtn.read();
+  Serial.println(pressCount);
 
-  bool isPressed = !digitalRead(PIN);
-  if (isPressed)
+  if (stickBtn.wasPressed())
   {
-    M5.Beep.beep();
-  }
-
-  delay(2);
+    uint32_t now_ms = millis();
+    if (now_ms - pressTime > MARGIN_TIME)
+    {
+      M5.Beep.beep();
+      pressTime = now_ms;
+      pressCount += 1;
+    };
+  };
 }
