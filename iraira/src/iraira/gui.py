4014f3465f6e606150bd7c64445c7ee5b056889d
GM <myrgksshuri@gmail.com>
2022-08-15 12:54:40 +0900

    整理

--- iraira/src/iraira/gui.py
+++ iraira/src/iraira/gui.py
@@ -10,30 +10,39 @@ class Application(tk.Frame):
     def __init__(self, master: tk.Tk, app_state: AppState, sig_param: SignalParam, player_param: PlayerState):
         super().__init__(master)
 
-        self.app_state = app_state
-        self.sig_param = sig_param
-        self.player_param = player_param
+        self._app_state = app_state
+        self._sig_param = sig_param
+        self._player_param = player_param
 
         self.check_close()
 
         self.master = master
-        self.master.geometry("600x600")
-        self.master.protocol("WM_DELETE_WINDOW", self.delete_window)
+        self.master.geometry("800x600")
+        self.master.title("イライラ棒")
         self.pack()
         self.create_widgets()
+        self.master.protocol("WM_DELETE_WINDOW", self.on_close_botton_click)
 
-    def delete_window(self) -> None:
-        self.master.destroy()
-        self.app_state.is_running = False
+    def check_close(self) -> None:
+        """アプリの起動状態監視と終了処理"""
+        if not self._app_state.is_running:
+            self.master.destroy()
+            return
+
+        self.after(1000, self.check_close)
+
+    def on_close_botton_click(self) -> None:
+        """ウィンドウの閉じる[x]ボタンを押したときの処理"""
+        self._app_state.is_running = False
 
     def create_widgets(self) -> None:
         # アプリ情報
         status_text = (
-            f"volume {self.player_param.volume:.1f}\n"
-            f"f: {self.sig_param.frequency:>4}\n"
-            f"traction: {self.sig_param.traction_direction:>4}\n"
-            f"count_anti_node: {self.sig_param.count_anti_node:>3}\n"
-            f"play: {'playing' if self.player_param.is_running else 'stop':>7}"
+            f"volume {self._player_param.volume:.1f}\n"
+            f"f: {self._sig_param.frequency:>4}\n"
+            f"traction: {self._sig_param.traction_direction:>4}\n"
+            f"count_anti_node: {self._sig_param.count_anti_node:>3}\n"
+            f"play: {'playing' if self._player_param.is_running else 'stop':>7}"
         )
         self.status = tk.Label(self, text=status_text, font=(None, 24), width="100", anchor=tk.W)
         self.status.pack()
@@ -49,11 +58,11 @@ class Application(tk.Frame):
     def show_status(self) -> None:
         self.status.configure(
             text=(
-                f"volume {self.player_param.volume:.1f}\n"
-                f"f: {self.sig_param.frequency:>4}\n"
-                f"traction: {self.sig_param.traction_direction:>4}\n"
-                f"count_anti_node: {self.sig_param.count_anti_node:>3}\n"
-                f"play: {'playing' if self.player_param.is_running else 'stop':>7}"
+                f"volume {self._player_param.volume:.1f}\n"
+                f"f: {self._sig_param.frequency:>4}\n"
+                f"traction: {self._sig_param.traction_direction:>4}\n"
+                f"count_anti_node: {self._sig_param.count_anti_node:>3}\n"
+                f"play: {'playing' if self._player_param.is_running else 'stop':>7}"
             )
         )
 
@@ -65,23 +74,18 @@ class Application(tk.Frame):
         key_name = event.keysym
         self.key_value.set(key_name)
 
-        if event.keysym_num == 32:  # key: space
-            self.player_param.change_play_state()
+        if event.keysym_num == 113:  # key: q
+            self._app_state.is_running = False
+        elif event.keysym_num == 32:  # key: space
+            self._player_param.change_play_state()
 
         elif event.keysym_num == 65361:  # key: Left
-            self.sig_param.traction_up()
+            self._sig_param.traction_up()
         elif event.keysym_num == 65363:  # key: Right
-            self.sig_param.traction_down()
-
-    def check_close(self) -> None:
-        """アプリの終了監視"""
-        if not self.app_state.is_running:
-            self.master.destroy()
-        self.after(1000, self.check_close)
+            self._sig_param.traction_down()
 
 
 def show_gui(app_state: AppState, player_param: PlayerState, sig_param: SignalParam) -> None:
     """アプリGUI画面を表示する"""
-    root = tk.Tk()
-    app = Application(root, app_state, sig_param, player_param)
+    app = Application(tk.Tk(), app_state, sig_param, player_param)
     app.mainloop()
