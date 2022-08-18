c709740974208cad0545afc15efe434f8a30c5ab
GM <myrgksshuri@gmail.com>
2022-08-19 03:48:29 +0900

    change pin name

--- iraira/src/iraira/gpio_raspi.py
+++ iraira/src/iraira/gpio_raspi.py
@@ -4,10 +4,10 @@ import RPi.GPIO as GPIO
 from iraira.player import SignalParam
 from iraira.state import AppState
 
-GPIO_PIN = 17
+PIN_TRRACTION_CHANGE = 17
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PIN_TRRACTION_CHANGE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def switch_listener(
@@ -15,5 +15,5 @@ def switch_listener(
     sig_param: SignalParam,
 ) -> None:
     while app_state.is_running:
-        _ = GPIO.wait_for_edge(GPIO_PIN, GPIO.FALLING, bouncetime=500)
+        _ = GPIO.wait_for_edge(PIN_TRRACTION_CHANGE, GPIO.FALLING, bouncetime=500)
         sig_param.traction_change()
