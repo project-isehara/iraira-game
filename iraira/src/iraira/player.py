656cd81f65970cee7f4e106be9d7860995bd1666
GM <myrgksshuri@gmail.com>
2022-09-02 18:56:32 +0900

    音調整
    壁タッチ音 0.5秒
    音量調整

--- iraira/src/iraira/player.py
+++ iraira/src/iraira/player.py
@@ -15,32 +15,43 @@ from iraira.traction_wave import traction_wave
 from iraira.util import RepoPath
 
 _assert_path = RepoPath().assert_dir
-_sound_touch_wall_1_path = _assert_path / "maouaudio/魔王魂  戦闘09.wav"
-_sound_touch_wall_2_path = _assert_path / "効果音ラボ/大砲2.wav"
-_sound_touch_wall_3_path = _assert_path / "効果音ラボ/爆発2.wav"
-_sound_goal_path = _assert_path / "nakano sound/ファンファーレ6（戦闘勝利＋BGM）.wav"
+_sound_touch_wall_1_path = _assert_path / "効果音ラボ/大砲2.wav"  # 44.1 kHz
+_sound_touch_wall_2_path = _assert_path / "効果音ラボ/爆発2.wav"  # 44.1 kHz
+# _sound_touch_wall_2_path = _assert_path / "maouaudio/魔王魂  戦闘09.wav"  # 48.0 kHz
+_sound_goal_path = _assert_path / "nakano sound/ファンファーレ6（戦闘勝利＋BGM）.wav"  # 44.1 kHz
 
 
-def read_wav(file: Path) -> npt.NDArray[np.float32]:
+def read_wav(file: Path) -> npt.NDArray[np.uint8 | np.int16]:
     with wave.open(str(file), "rb") as fr:
+        channel = fr.getnchannels()
+        sample_width = fr.getsampwidth()
         frame = fr.readframes(fr.getnframes())
 
-    return np.frombuffer(frame, dtype=np.float32)
+    if sample_width == 1:
+        d_type = np.uint8
+    elif sample_width == 2:
+        d_type = np.int16
+    else:
+        raise NotImplementedError()
+
+    w = np.frombuffer(frame, dtype=d_type)
+    if channel == 2:
+        return w[::2]
+    return w
 
 
 class GameSoundEffect:
     def __init__(self) -> None:
         self._sound_touch_walls = (
-            read_wav(_sound_touch_wall_1_path),
-            read_wav(_sound_touch_wall_2_path),
-            read_wav(_sound_touch_wall_3_path),
+            (read_wav(_sound_touch_wall_1_path)[:22050] * 2).astype(np.int16),  # 0.5 sec & 音量調整
+            (read_wav(_sound_touch_wall_2_path)[:22050] * 2).astype(np.int16),  # 0.5 sec & 音量調整
         )
         self._sound_goal = read_wav(_sound_goal_path)
 
-    def sound_touch_wall_random(self) -> npt.NDArray[np.float32]:
+    def sound_touch_wall_random(self) -> npt.NDArray[np.int16]:
         return random.choice(self._sound_touch_walls)
 
-    def sound_goal(self) -> npt.NDArray[np.float32]:
+    def sound_goal(self) -> npt.NDArray[np.int16]:
         return self._sound_goal
 
 
@@ -49,7 +60,7 @@ class Player:
 
     def __init__(self, param: PlayerState):
         self._py_audio = pyaudio.PyAudio()
-        self._stream = self._py_audio.open(format=pyaudio.paFloat32, channels=1, rate=param.fs, output=True)
+        self._stream = self._py_audio.open(format=pyaudio.paInt16, channels=1, rate=param.fs, output=True)
         self.param = param
 
     def start(self) -> None:
@@ -68,7 +79,7 @@ class Player:
         self._stream.close()
         self._py_audio.terminate()
 
-    def write(self, sig: npt.NDArray[np.float32]) -> None:
+    def write(self, sig: npt.NDArray[np.int16]) -> None:
         self._stream.write(sig.tobytes())
 
     def __enter__(self) -> Player:
@@ -84,7 +95,7 @@ def create_traction_wave(
     frequency: int,
     traction_direction: TractionDirection,
     count_anti_node: int = 4,
-) -> npt.NDArray[np.float32]:
+) -> npt.NDArray[np.float_]:
     """牽引力信号を生成する
 
     :param fs: サンプリング周波数[Hz]
@@ -100,7 +111,7 @@ def create_traction_wave(
 
     # 波形生成
     t = np.linspace(0, duration_sec, int(fs * duration_sec), endpoint=False)
-    sig = traction_wave(2 * np.pi * frequency * t, count_anti_node).astype(np.float32)
+    sig = traction_wave(2 * np.pi * frequency * t, count_anti_node)
 
     # 牽引力方向の調整
     if traction_direction == TractionDirection.down:
@@ -142,9 +153,10 @@ def play(app_state: AppState, player_param: PlayerState, sig_param: SignalParam,
                         sig_param.traction_direction,
                         sig_param.count_anti_node,
                     )
-                    sig = traction_wave * player_param.volume
 
-                touch_count = game_state.touch_count
+                    # 値域調整 16bit & 音量調整
+                    sig = (traction_wave * 32767 * player_param.volume).astype(np.int16)
+
                 player.write(sig)
 
     except Exception as e:
